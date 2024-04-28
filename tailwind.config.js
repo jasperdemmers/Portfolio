4154c7b498dc5cae4c0f15a735e1f97d5e6c7365
jasperdemmers <jasper.demmers@hotmail.com>
2024-04-28 18:16:37 +0200

    re-arranged

--- tailwind.config.js
+++ tailwind.config.js
@@ -35,8 +35,8 @@ export default {
       },
     },
     screens: {
-      "sm": "640px",
       "xs": "475px",
+      "sm": "640px",
       "md": "768px",
       "lg": "1024px",
       "xl": "1280px",
