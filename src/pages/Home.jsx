d1124f41f625968ec53a923184ec2aae3df3ba51
jasperdemmers <jasper.demmers@hotmail.com>
2024-04-28 20:22:34 +0200

    home/hero

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,8 +8,8 @@ const Home = () => {
 
   return (
     <div className='text-text-light dark:text-text-dark'>
-      <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full' onClick={toggleDark}>Toggle Dark Mode</button>
       <Hero />
+      <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full' onClick={toggleDark}>Toggle Dark Mode</button>
     </div>
   )
 }
--- src/sections/home/Hero.jsx
+++ src/sections/home/Hero.jsx
@@ -1,21 +1,64 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { useEffect, useState } from "react";
+
 const Hero = () => {
-  return (
-    <div className="container flex mx-auto justify-between items-center">
-        {/* Left hand text */}
-        <div>
-            <span>HELLO WORLD</span>
-            <br />
-            <span>ITS ME,</span>
-            <br />
-            <span
-                className="font-display xs:text-xl sm:text-2xl md:text-4xl lg:text-5xl xl:text-6xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-primary-light dark:from-primary-dark to-accent-light dark:to-accent-dark"
-            >
-                JASPER DEMMERS
-            </span>
-            
+    const [iconColor, setIconColor] = useState(determineInitialIconColor());
+
+    function determineInitialIconColor() {
+        return document.documentElement.classList.contains('dark') 
+            ? { github: '#EBEBEB', linkedin: '#EBEBEB' } 
+            : { github: '#232930', linkedin: '#0D67C2' };
+    }
+
+    useEffect(() => {
+        const themeChangeObserver = new MutationObserver((mutations) => {
+            mutations.forEach((mutation) => {
+                if (mutation.attributeName === 'class') {
+                    const isDarkMode = document.documentElement.classList.contains('dark');
+                    setIconColor(isDarkMode ? { github: '#EBEBEB', linkedin: '#EBEBEB' } : { github: '#232930', linkedin: '#0D67C2' });
+                }
+            });
+        });
+
+        themeChangeObserver.observe(document.documentElement, {
+            attributes: true
+        });
+
+        return () => themeChangeObserver.disconnect();
+    }, []);
+
+    return (
+        <div className="px-4 flex flex-col md:flex-row items-center gap-4 md:pt-12">
+            {/* Left hand text */}
+            <div className="flex-1">
+                <div className="tracking-wide font-body">
+                    <span>HELLO WORLD</span>
+                    <br />
+                    <span>ITS ME,</span>
+                </div>
+                <span
+                    className="tracking-wide font-display text-4xl lg:text-5xl xl:text-6xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-primary-light dark:from-primary-dark to-accent-light dark:to-accent-dark"
+                >
+                    JASPER DEMMERS
+                </span>
+                <br />
+                <span className="tracking-wide break-normal font-body antialiased">
+                    I am not just living in the era of technology, I am actively shaping it.
+                </span>
+                <div className='mt-4 flex justify-start gap-4 pt-0 md:pt-8'>
+                    <a>
+                        <FaLinkedin className="w-12 h-12" style={{ color: iconColor.linkedin }}/>
+                    </a>
+                    <a>
+                        <FaGithub className="w-12 h-12" style={{ color: iconColor.github }}/>
+                    </a>
+                </div>
+            </div>
+            <div className="flex-1">
+                <img className="border-8 rounded-3xl border-accent-light dark:border-accent-dark" src="/portraits/straight-look.png"></img>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Hero
