37559a26adb927d84ac2f5eb9ecc1fd132f2ba1f
jasperdemmers <jasper.demmers@hotmail.com>
2024-04-29 11:26:31 +0200

    Removed unused function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,6 @@
 import Hero from '../sections/home/Hero'
 
 const Home = () => {
-  // add a function to toggle the classList of dark
-  const toggleDark = () => {
-    document.documentElement.classList.toggle('dark')
-  }
-
   return (
     <div className='text-text-light dark:text-text-dark mx-auto'>
       <Hero />
