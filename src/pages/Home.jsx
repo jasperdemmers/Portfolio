ffc3f8fe2276732543c469e496c1a46b5645f5b9
jasperdemmers <jasper.demmers@hotmail.com>
2024-05-22 12:19:49 +0200

    Added Whoami section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from '../sections/home/Hero'
+import Whoami from '../sections/home/Whoami'
 
 const Home = () => {
-  return (
-    <div className='text-text-light dark:text-text-dark mx-auto'>
-      <Hero />
-    </div>
-  )
+    return (
+        <div className='text-text-light dark:text-text-dark mx-auto'>
+            <Hero />
+            <Whoami />
+        </div>
+    )
 }
 
 export default Home
