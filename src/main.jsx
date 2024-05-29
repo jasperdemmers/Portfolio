e19c61e1b921d6f809294747a1d5b27699fde6a2
jasperdemmers <jasper.demmers@hotmail.com>
2024-05-29 21:04:14 +0200

    404 page

--- src/main.jsx
+++ src/main.jsx
@@ -3,28 +3,56 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css'
 import Home from './pages/Home' 
 import About from './pages/About'
-import Profile from './pages/Profile'
-import Work from './pages/Work'
-import Contact from './pages/Contact'
+// import Profile from './pages/Profile'
+// import Work from './pages/Work'
+// import Contact from './pages/Contact'
 import NoPage from './pages/NoPage'
 import Navbar from './components/Navbar';
 import ThemeButton from './components/ThemeButton';
 import ScrollToTop from "./components/ScrollToTop.js";
 
+const Overlay = () => {
+    return (
+        <div className={"constrained-container"}>
+            <Navbar/>
+            <ThemeButton/>
+        </div>
+    )
+}
+
 export default function App() {
     return (
         <BrowserRouter>
-            <ScrollToTop />
-            <div className={"constrained-container"}>
-                <Navbar />
-                <ThemeButton />
-            </div>
+            <ScrollToTop/>
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="whoami" element={<About />} />
-                <Route path="profile" element={<Profile />} />
-                <Route path="bin" element={<Work />} />
-                <Route path="ping" element={<Contact />} />
+                <Route path="/" element={
+                    <>
+                        <Overlay />
+                        <Home />
+                    </>} />
+                <Route path="whoami" element={
+                    <>
+                        <Overlay />
+                        <About />
+                    </>} />
+                <Route path="profile" element={
+                    <>
+                        <NoPage/>
+                        {/*<Overlay />*/}
+                        {/*<Profile />*/}
+                    </>} />
+                <Route path="bin" element={
+                    <>
+                        <NoPage/>
+                        {/*<Overlay />*/}
+                        {/*<Work />*/}
+                    </>} />
+                <Route path="ping" element={
+                    <>
+                        <NoPage/>
+                        {/*<Overlay />*/}
+                        {/*<Contact />*/}
+                    </>} />
                 <Route path="*" element={<NoPage />} />
             </Routes>
         </BrowserRouter>
--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -1,7 +1,15 @@
 const NoPage = () => {
-  return (
-    <h1>404</h1>
-  )
+    return (
+        <div className="min-h-screen flex flex-grow items-center justify-center">
+            <div className="rounded-lg bg-white dark:bg-secondary-light p-8 text-center shadow-xl">
+                <h1 className="mb-4 text-4xl font-bold text-text-light dark:text-text-dark">404</h1>
+                <p className="text-text-light dark:text-text-dark">Oops! The page you are looking for could not be found.</p>
+                <a href="/"
+                     className="mt-4 inline-block rounded bg-blue-500 px-4 py-2 font-semibold text-white hover:bg-blue-600"> Go
+                    back to Home </a>
+              </div>
+        </div>
+    )
 }
 
 export default NoPage
